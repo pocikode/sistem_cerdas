cde62e71e05c43fe1724483836c57ff9796bb05f
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2023-12-30 17:28:46 +0700

    🔧 chore(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics ✨ feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port 🔧 chore(urls.py): add new path for check view ✨ feat(views.py): add check view to handle sentiment analysis of a given text 🔧 chore(requirements.txt): add click, joblib, nltk, numpy, pandas, regex, scikit-learn, scipy, threadpoolctl, tqdm, python-dateutil, pytz, tzdata packages 🔧 chore(stopwordbahasa.csv): add stopwordbahasa.csv file 🔧 chore(Sentiment-SVC.pickle): add Sentiment-SVC.pickle model file 🔧 chore(vectoriser-ngram-(1,2).pickle): add vectoriser-ngram-(1,2).pickle model file 🔧 chore(base.html): add Alpine.js script tag ✨ feat(tools.html): add Alpine.js functionality to handle sentiment analysis form submission and display result

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,5 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('tools', views.tools, name='tools'),
+    path('check', views.check, name='check'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,91 @@
+import json
+import pickle
+import re
+
+import pandas as pd
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from nltk import WordNetLemmatizer
 
 from dashboard.models import Sentiment
+from web.settings import BASE_DIR
 
+
+def preprocess(textdata):
+    processedText = []
+
+    # Create Lemmatizer and Stemmer.
+    word_lemm = WordNetLemmatizer()
+
+    # Defining regex patterns.
+    url_pattern = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    user_pattern = '@[^\s]+'
+    alpha_pattern = "[^a-zA-Z0-9]"
+    sequence_pattern = r"(.)\1\1+"
+    seq_replace_pattern = r"\1\1"
+
+    for tweet in textdata:
+        # Defining dictionary containing all emojis with their meanings.
+        emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad',
+                  ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+                  ':-@': 'shocked', ':@': 'shocked', ':-$': 'confused', ':\\': 'annoyed',
+                  ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+                  '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+                  '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink',
+                  ';-)': 'wink', 'O:-)': 'angel', 'O*-)': 'angel', '(:-D': 'gossip', '=^.^=': 'cat'}
+
+        ## Defining set containing all stopwords in Bahasa Indonesia.
+        stopword_df = pd.read_csv(BASE_DIR / 'static/csv/stopwordbahasa.csv', header=None)
+        stopwordlist = stopword_df[0].to_list()
+
+        tweet = tweet.lower()
+
+        # Replace all URls with 'URL'
+        tweet = re.sub(url_pattern, ' URL', tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])
+            # Replace @USERNAME to 'USER'.
+        tweet = re.sub(user_pattern, ' USER', tweet)
+        # Replace all non alphabets.
+        tweet = re.sub(alpha_pattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequence_pattern, seq_replace_pattern, tweet)
+
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+            # if word not in stopwordlist:
+            if len(word) > 1:
+                # Lemmatizing the word.
+                word = word_lemm.lemmatize(word)
+                tweetwords += (word + ' ')
+
+        processedText.append(tweetwords)
+
+    return processedText
+
+
+def load_model():
+    file = open(BASE_DIR / 'static/models/vectoriser-ngram-(1,2).pickle', 'rb')
+    vectoriser = pickle.load(file)
+    file.close()
+
+    file = open(BASE_DIR / 'static/models/Sentiment-SVC.pickle', 'rb')
+    model = pickle.load(file)
+    file.close()
+
+    return vectoriser, model
+
+
+def predict(text):
+    vectoriser, model = load_model()
+
+    vector = vectoriser.transform(preprocess(text))
+    sentiment = model.predict(vector)
+
+    return sentiment.tolist()
 
 # Create your views here.
 def index(request):
@@ -19,3 +103,16 @@ def index(request):
 
 def tools(request):
     return render(request, 'tools.html')
+
+
+@csrf_exempt
+def check(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        sentiment_label = ['Negative', 'Neutral', 'Positive']
+        sentiment = predict([data['text']])[0]
+
+        return JsonResponse({
+            'result': sentiment_label[sentiment]
+        })
