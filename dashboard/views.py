93723ee6fe413a5a847d728f6b7a2d45352e61e1
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2023-12-30 16:20:00 +0700

    🔧 fix(views.py): calculate and pass sentiment totals to the index template for display
    🔧 fix(index.html): update the display of sentiment totals to use the calculated values from the view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+from dashboard.models import Sentiment
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    sentiment = Sentiment.objects.all()
+    total_negative = sum(entry.total for entry in sentiment if entry.label == 'negative')
+    total_neutral = sum(entry.total for entry in sentiment if entry.label == 'neutral')
+    total_positive = sum(entry.total for entry in sentiment if entry.label == 'positive')
+
+    return render(request, 'index.html', {
+        'total_negative': total_negative,
+        'total_neutral': total_neutral,
+        'total_positive': total_positive
+    })
 
 
 def tools(request):
